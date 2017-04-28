168b3ef1eeb2669625a031851433662dc96f3002
codinghobby@gmail.com <codinghobby@gmail.com>
2017-04-28 16:00:00 +0200

    Fixed error

--- JavaScript/Video-Capture/app/home/app.js
+++ JavaScript/Video-Capture/app/home/app.js
@@ -6,9 +6,8 @@ let video = new Element('video')
 	.attr('autoplay', true)
 
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-	// Not adding `{ audio: true }` since we only want video now
 	navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
-		video.src = window.URL.createObjectURL(stream);
-		video.play();
+		video.attr('src', window.URL.createObjectURL(stream));
+		video.toNode().play();
 	})
 }
